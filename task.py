628b0a12632d00f05d71935ed3b7064a78aac749
Ethank-lcwc <144791953+Ethank-lcwc@users.noreply.github.com>
2024-09-30 12:50:37 +0000

    cleverrrrr

--- task.py
+++ task.py
@@ -1,1 +1,31 @@
-# You will define and write each function for the task in this file
+def temperature_converter(temp,unit):
+    if unit =="C":
+        temp = (temp * 1.8) + 32
+        unit = " F"
+    elif unit == "F":
+        temp = (temp - 32) / 1.8
+        unit = " C"
+    temp = int(temp)
+    temp = str(temp)
+    return (temp + unit)
+
+def reverse(string):
+    string = string [::-1]
+    return string
+
+def Fibonacci(N):
+    count = 0
+    hold = 0
+    num1 = 0
+    num2 = 1
+    if N == 0:
+        return N
+    else:
+        while count < (N - 1):
+            hold = num1 + num2
+            num1 = num2
+            num2 = hold
+            count = count + 1
+        return num2
+
+
